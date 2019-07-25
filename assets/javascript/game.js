490855986329b4f6fbb9ac534dac36c21a1b8966
Guillermo Villalta <mexcelus@gmail.com>
2019-07-25 15:21:30 -0600

    added logic to color green selected character and color red enemies

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,9 +6,13 @@ $('.character').click(function () {
     $('.character').each( function() {
         if ($(this).html() === htmlString) {
             console.log('hey');
+            $(this).find('h6').css('background', '#008000');
+            console.log($(this).html());
             $('#selected-container').append($(this).html());
         } else {
             console.log('not');
+            $(this).find('h6').css('background', '#B22222');
+            console.log($(this).html());
             $('#enemies-container').append($(this).html());
         }
     })
