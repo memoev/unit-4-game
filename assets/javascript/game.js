43f51e2a62e8b52faa732141f9244ed2d6503671
Guillermo Villalta <mexcelus@gmail.com>
2019-07-25 09:47:18 -0600

    added logic to place character selected and enemies inside elements when clicked

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,17 @@
-$('.character-container').click(function () {
+$('.character').click(function () {
     let htmlString = $(this).html();
+    //console.log(htmlString);
+    //console.log($('#character-box').not($(this)).html());
+    
+    $('.character').each( function() {
+        if ($(this).html() === htmlString) {
+            console.log('hey');
+            $('#selected-container').append($(this).html());
+        } else {
+            console.log('not');
+            $('#enemies-container').append($(this).html());
+        }
+    })
+    
     $('#character-box').empty();
-
-    $('#selected-character').append(htmlString);
 });
